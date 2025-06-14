1564aabf56a2604cbaa652140825895869619533
kunkky <83945048+kunkky@users.noreply.github.com>
2025-06-14 09:39:54 +0100

    Update BaseUrl.ts

--- frontend/src/utils/BaseUrl.ts
+++ frontend/src/utils/BaseUrl.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://indoorheatpump.com/mood/backend/";
-export const LOCAL_BASE_URL = "http://localhost/teampulse/backend/";
+export const BASE_URL = "https://indoorheatpump.com/mood/backend";
+export const LOCAL_BASE_URL = "http://localhost/teampulse/backend";
